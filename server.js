21cb1b658969dcbbf143c330a83285f5b4d99f0e
Shane Guittap <shaneguittap@Kristins-MacBook-Pro-2.local>
2018-10-09 16:51:47 -0600

    added route/index/changed colors

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const pokemon = require('./modals/pokemon')
 
-app.get('/pokemon', (req, res)=>{
-    res.send(pokemon)
+app.get('/pokemon/:index', (req, res)=>{
+    res.render('index.ejs',{
+        pokemon: pokemon
+    })
 })
 
 
